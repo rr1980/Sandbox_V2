c1cba3378ebb6714cf55cc710a2e58a77adaf5fc
rr1980 <rener1980@googlemail.com>
2017-11-04 00:40:17 +0100

    logger

--- RR.LoggerService/DebugLoggerService/DebugLoggerAction.cs
+++ RR.LoggerService/DebugLoggerService/DebugLoggerAction.cs
@@ -13,35 +13,49 @@ namespace RR.LoggerService.DebugLoggerService
 
         public DebugLoggerAction(string name, DebugLoggerConfiguration loggerConfiguration, ILogger selfLogger)
         {
-            #region throwExceptions
-
-            if (string.IsNullOrEmpty(name))
+            try
             {
-                throw new ArgumentNullException("name");
-            }
+                #region throwExceptions
 
-            if (loggerConfiguration == null)
+                if (string.IsNullOrEmpty(name))
+                {
+                    throw new ArgumentNullException("name");
+                }
+
+                if (loggerConfiguration == null)
+                {
+                    throw new ArgumentNullException("loggerConfiguration");
+                }
+
+                if (selfLogger == null)
+                {
+                    throw new ArgumentNullException("selfLogger");
+                }
+
+                #endregion throwExceptions
+
+                _name = name;
+                _loggerConfiguration = loggerConfiguration;
+                _selfLogger = selfLogger;
+                _selfLogger.LogTrace("DebugLoggerAction init finish for: '" + _name + "'");
+            }
+            catch (Exception ex)
             {
-                throw new ArgumentNullException("loggerConfiguration");
+                throw new DebugLoggerException("DebugLoggerAction ctor failed for: " + name, ex);
             }
-
-            if (selfLogger == null)
-            {
-                throw new ArgumentNullException("selfLogger");
-            }
-
-            #endregion throwExceptions
-
-            _name = name;
-            _loggerConfiguration = loggerConfiguration;
-            _selfLogger = selfLogger;
-            _selfLogger.LogTrace("DebugLoggerAction init finish for: '" + _name + "'");
         }
 
         public void Log<TState>(string categoryName, LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            Debug.WriteLine(DateTime.Now + " " + logLevel + " : " + categoryName + " : " + formatter(state, exception));
-            _selfLogger.LogTrace("DebugLoggerAction Log run: '" + categoryName + "'");
+            try
+            {
+                Debug.WriteLine(DateTime.Now + " " + logLevel + " : " + categoryName + " : " + formatter(state, exception));
+                _selfLogger.LogTrace("DebugLoggerAction Log run: '" + categoryName + "'");
+            }
+            catch (Exception ex)
+            {
+                throw new DebugLoggerException("DebugLoggerAction Log failed for: " + categoryName, ex);
+            }
         }
     }
 }
